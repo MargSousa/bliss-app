3cbc20fdedd570d91a964c15f2f6fb0b09e62708
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2021-07-29 21:28:59 +0100

    Added loading  spinner, server check and retry button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Loader from "react-loader-spinner";
+import QuestionsList from './components/QuestionsList';
 import './App.css';
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [serverHealth, setServerHealth] = useState(false);
+
+  useEffect(() => {
+    getServerHealth();
+  }, [])
+
+  const getServerHealth = () => {
+    axios.get('https://private-anon-7c54611a93-blissrecruitmentapi.apiary-mock.com/health')
+    .then((res) => {
+      if(res.data.status === 'OK') {
+        setServerHealth(true);
+        setIsLoading(false);
+      } else {
+        setIsLoading(false);
+        setServerHealth(false);
+      }
+    }
+  )}
+
+  const handleRetryClick  = () => {
+    setIsLoading(true);
+    getServerHealth();
+  }
 
   return (
     <div className="App">
       <div className="main-title">Bliss Recruitment App</div>
       <div className="main-box">
-        <div>Hello world</div>
+        { isLoading && 
+          <Loader type="Circles" color="#95D7BD" height={60} width={60} />
+        }
+        { !isLoading && !serverHealth &&
+          <button className="btn btn-retry" onClick={handleRetryClick}>Retry Action</button>
+        }
+        { !isLoading && serverHealth &&
+          <QuestionsList />
+        }
       </div>
     </div>
   );
---
+++ src/components/QuestionsList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const QuestionsList = () => {
+  return (
+    <div>
+      <div>Question</div>
+      <div>Question</div>
+      <div>Question</div>
+    </div>
+  );
+}
+ 
+export default QuestionsList;
